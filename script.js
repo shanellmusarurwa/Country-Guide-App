02b7ec1ead4975cd58ab84c2f630586f5a0ecccd
shanellmusarurwa <155754152+shanellmusarurwa@users.noreply.github.com>
2024-02-29 19:58:47 +0200

    injected some html in js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 searchBtn.addEventListener("click", () => {
-  let counrtyName = "India";
+  let counrtyName = countryInp.value;
   let finalURL = `https://restcountries.com/v3.1/name/${counrtyName}?fullText=true`;
   console.log(finalURL);
   fetch(finalURL)
@@ -17,7 +17,41 @@ searchBtn.addEventListener("click", () => {
       console.log(
         Object.values(data[0].languages).toString().split(",").join(",")
       );
-      result.innerHTML = `<img src=" ${data[0].flags.svg} class="flag-img"> <h2>${data[0].name.common}</h2> <div class="wrapper"><div class="data-wrapper"></div>
+      result.innerHTML = `<img src=" ${data[0].flags.svg} class="flag-img"> <h2>${data[0].name.common}</h2> <div class="wrapper"><div class="data-wrapper"><h4>Capital:</h4><span>${data[0].capital[0]}</span></div>
       </div>`;
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Continent:</h4>
+          <span>${data[0].continents[0]}</span>
+        </div>
+      </div>;
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Population:</h4>
+          <span>${data[0].population}</span>
+        </div>
+      </div>;
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Currency:</h4>
+          <span>
+            ${data[0].currencies[Object.keys(data[0].currencies)].name} - $
+            {Object.keys(data[0].currencies)[0]}
+          </span>
+        </div>
+      </div>;
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Common Language:</h4>
+          <span>${data[0].languages.toString().split(",").join(",")}</span>
+        </div>
+      </div>;
+    })
+    .catch(() => {
+      if (counrtyName.length == 0) {
+        result.innerHTML = `<h3>The input field can not be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
     });
 });
