c231370f5fdea08854803bab93cabca405b0fa6a
shanellmusarurwa <155754152+shanellmusarurwa@users.noreply.github.com>
2024-02-29 16:16:52 +0200

    added a fetch on js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 searchBtn.addEventListener("click", () => {
-  let counrtyName = countryInp.value;
+  let counrtyName = "India";
   let finalURL = `https://restcountries.com/v3.1/name/${counrtyName}?fullText=true`;
   console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      console.log(data[0].capital[0]);
+      console.log(data[0].flags.svg);
+      console.log(data[0].name.common);
+      console.log(data[0].continents[0]);
+      console.log(Object.keys(data[0].currencies)[0]);
+      console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+      console.log(
+        Object.values(data[0].languages).toString().split(",").join(",")
+      );
+      result.innerHTML = `<img src=" ${data[0].flags.svg} class="flag-img">`;
+    });
 });
